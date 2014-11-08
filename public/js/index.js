10b8b3e113b2e39688d2c2689cba81437e114ca2
Matej Simek <email@matejsimek.com>
2014-11-08 19:29:30 +0100

    Cleared index.js of map debug calls

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,6 @@
 var plugins = require('./plugins')
 var React = require('react')
 
-setTimeout(function() {
-	map.setAds([]);
-}, 2000);
-
 var router = require('../components/router.jsx')
 React.renderComponent(router, document.body);
 
