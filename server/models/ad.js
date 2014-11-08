7f83be3cc8e70191240e946259a6414c95465a31
Jiří Špác <capajj@gmail.com>
2014-11-08 12:15:35 +0100

    location now required

--- server/models/ad.js
+++ server/models/ad.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 module.exports = new mongoose.Schema({
-	location: { type: [Number], index: '2dsphere'},
+	location: { type: [Number], index: '2dsphere', required: true},
 	creation_date: { type: Date, default: Date.now },
 	date: { type: Date, default: Date.now },
 	text: { type: String },
