99b121ce53029f68e230e69633252505bfd3ab8c
Matej Simek <email@matejsimek.com>
2014-11-08 20:08:58 +0100

    Markers have ids

--- public/components/map.jsx
+++ public/components/map.jsx
@@ -9,12 +9,12 @@ var geoJumper = require('../js/geoJumper')
 var PRAHA = {coordinates: [50.051611, 14.407032], type:"Point"}
 var BRNO = {coordinates: [49.19163, 16.61238], type:"Point"}
 var ADS = [
-	{location: {coordinates: [50.06516, 14.41738], type:"Point"}, radius: 1000},
-	{location: {coordinates: [50.060622, 14.389032], type:"Point"}, radius: 500},
-	{location: {coordinates: [50.05789, 14.43751], type:"Point"}, radius: 1000},
-	{location: {coordinates: [50.04052, 14.36838], type:"Point"}, radius: 2000},
-	{location: {coordinates: [49.19068, 16.65304], type:"Point"}, radius: 1000},
-	{location: {coordinates: [49.2033, 16.57416], type:"Point"}, radius: 500}
+	{ id: 1, location: {coordinates: [50.06516, 14.41738], type:"Point"}, radius: 1000},
+	{ id: 2, location: {coordinates: [50.060622, 14.389032], type:"Point"}, radius: 500},
+	{ id: 3, location: {coordinates: [50.05789, 14.43751], type:"Point"}, radius: 1000},
+	{ id: 4, location: {coordinates: [50.04052, 14.36838], type:"Point"}, radius: 2000},
+	{ id: 5, location: {coordinates: [49.19068, 16.65304], type:"Point"}, radius: 1000},
+	{ id: 6, location: {coordinates: [49.2033, 16.57416], type:"Point"}, radius: 500}
 ];
 
 
