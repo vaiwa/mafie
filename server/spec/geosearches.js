4b8b84985b073698ef113ef8b775044eff8aff05
Jiří Špác <capajj@gmail.com>
2014-11-08 15:32:56 +0100

    just added infor about longitude and latitude

--- server/spec/geosearches.js
+++ server/spec/geosearches.js
@@ -1,3 +1,3 @@
 
-//to search 50000 metres around lan
+//to search 50000 metres around Longitude 50.05 and Latitude 14.42
 //http://localhost:8060/api/ads?conditions={ "location": { "$nearSphere": {"$geometry":{"type":"Point", "coordinates" : [ 50.05, 14.42 ]}, "$maxDistance": 50000}} }
