11996f2085d0af8ce16aada936dadcd7c397428b
Jiří Špác <capajj@gmail.com>
2014-11-08 18:00:58 +0100

    removed obsolete stuff from index.js

--- public/js/index.js
+++ public/js/index.js
@@ -18,34 +18,6 @@ var ADS = [
 map.setMyCoord(PRAHA.coordinates);
 map.setAds(ADS);
 
-setTimeout(function(){
-	// map.moveTo(BRNO); // example - uncoment for move to BRNO
-}, 4000);
-
-
-var geocoder = new google.maps.Geocoder();
-var addrInputEl = document.getElementById('geoJumper');
-
-addrInputEl.addEventListener('keydown', function codeAddress(ev) {
-		if (ev.keyCode === 13) {
-			var address = document.getElementById('geoJumper').value;
-			geocoder.geocode( { 'address': address}, function(results, status) {
-				if (status == google.maps.GeocoderStatus.OK) {
-					var loc = results[0].geometry.location;
-					map.moveTo([loc.lat(), loc.lng()]);
-					console.log("success");
-				} else {
-					alert('Geocode was not successful for the following reason: ' + status);
-				}
-			});
-		}
-
-	}
-);
-
-
-
-
 var router = require('../components/router.jsx')
 // React.renderComponent(router, document.body);
 
