504b099c8e1fd3e140470cd96c6f3df7c07befe9
Jiří Špác <capajj@gmail.com>
2014-11-08 17:33:26 +0100

    geoJumper into separated file

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 var plugins = require('./plugins');
 var map = require('./map.coffee')();
 
-var geocoder = new google.maps.Geocoder();
+require('./geoJumper')(map);
 
 var PRAHA = [50.051611, 14.407032]
 var BRNO = [49.19163, 16.61238]
@@ -17,31 +17,7 @@ var ADS = [
 map.setMyCoord(PRAHA);
 map.setAds(ADS);
 
-
-setTimeout(function(){
-	// map.moveTo(BRNO); // example - uncoment for move to BRNO
-}, 4000);
-
-
 var router = require('../components/router.jsx')
 // React.renderComponent(router, document.body);
 
 
-var addrInputEl = document.getElementById('geoJumper');
-
-addrInputEl.addEventListener('keydown', function codeAddress(ev) {
-		if (ev.keyCode === 13) {
-			var address = document.getElementById('geoJumper').value;
-			geocoder.geocode( { 'address': address}, function(results, status) {
-				if (status == google.maps.GeocoderStatus.OK) {
-					var loc = results[0].geometry.location;
-					map.moveTo([loc.lat(), loc.lng()]);
-					console.log("success");
-				} else {
-					alert('Geocode was not successful for the following reason: ' + status);
-				}
-			});
-		}
-
-	}
-);
