0325d9141af39a7d85de4b2af8d378c7cac6e039
Matej Simek <email@matejsimek.com>
2014-11-09 12:14:15 +0100

    Adding now slides up on map click

--- public/components/add.jsx
+++ public/components/add.jsx
@@ -61,6 +61,7 @@ var Add = React.createClass({
 		var classes = React.addons.classSet({
 			'show-form-top': this.state.hasLocation
 		})
+		var title = this.state.hasLocation ? "Vyplň údaje o aktivitě" : "Vyber umístění aktivity na mapě"
 		return (
 			<div>
 				<header className="site-header">
@@ -72,34 +73,33 @@ var Add = React.createClass({
 				<Map pins={[]} onMapClick={this.handleMapClick} onMarkerClick={this.handleMarkerClick} />
 
 				<div className={classes}>
-					
+
 						<div className="list_group__box--offer list-group">
 							<div className="list-group-item">
-								<h4 className="offer_title">Vyberte umístění aktivity na mapě</h4>
+								<h4 className="offer_title">{title}</h4>
 								<div className="col__list col-xs-6 col-sm-6">
 									<select ref="sport" className="form-control">
 										{sports.map(function(sport) {
 											return <option key={sport.id} value={sport.id}>{sport.title}</option>
 										})}
-									<input ref="date" className="form-control" type="date" step="1" required/>
-									<input ref="mail" className="form-control" type="text" placeholder="Kontakt" required/>
-									<input className="form-control btn btn-primary" type="submit" value="Vložit" onClick={this.postNewAd}/>
+									</select>
 							</div>
 							<div className="offer-primary-field"></div>
 							<div className="col__list col-xs-6 col-sm-6">
-								<input className="form-control" type="text" placeholder="Datum"/>
+								<input ref="date" className="form-control" type="date" placeholder="Termín" required />
 							</div>
 							<div className="clearfix"></div>
 							<div className="offer-filed--divider"></div>
 							<div className="col__list col-xs-6 col-sm-6">
-								<input className="form-control" type="text" placeholder="Kontakt"/>
+								<input ref="mail" className="form-control" type="email" placeholder="E-mail" reqired/>
 							</div>
 							<div className="col__list col-xs-6 col-sm-6">
-								<input className="form-control btn btn-primary" type="submit" value="Vložit"/>
+								<input className="form-control btn btn-primary" type="submit" value="Vložit" onClick={this.postNewAd}/>
 							</div>
 							<div className="clearfix"></div>
 						</div>
-					
+					</div>
+
 				</div>
 
 			</div>
