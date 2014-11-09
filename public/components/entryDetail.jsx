938c06bb182d8714c132efacd483e3a6e957fe10
Jiří Špác <capajj@gmail.com>
2014-11-09 12:53:41 +0100

    detail fixed

--- public/components/entryDetail.jsx
+++ public/components/entryDetail.jsx
@@ -16,10 +16,25 @@ var EntryDetail = React.createClass({
 		e.preventDefault()
 		this.props.onSubmit(this.props.entry, this.refs.note.getDOMNode().value)
 	},
+	formatDate: function(date) {
+		var months = 'leden_únor_březen_duben_květen_červen_červenec_srpen_září_říjen_listopad_prosinec'.split('_');
+		date = new Date(date);
+		var curr_date = date.getDate();
+		var curr_month = date.getMonth();
+		var curr_year = date.getFullYear();
+
+		return curr_date + ". " + months[curr_month]+ " " + curr_year;
+	},
+
+	formatPos: function(pos) {
+		return pos[0] + ', ' + pos[1];
+	},
 
 	render: function() {
 		if(!this.props.entry) return null
 
+		var date = this.formatDate(this.props.entry.date);
+		var pos = this.formatPos(this.props.entry.location.coordinates)
 		var classes = this.props.classes + ' list-group-item'
 		var title = sports[this.props.entry.sport].title
 
@@ -32,8 +47,9 @@ var EntryDetail = React.createClass({
 				</h4>
 
 				<div className="list-group-item-text">
-					<p className="entry-info"><span className="glyphicon glyphicon-map-marker"></span> {this.props.entry.place}</p>
-					<p className="entry-info"><span className="glyphicon glyphicon-calendar"></span> {this.props.entry.time}</p>
+					<p className="entry-info"><span className="glyphicon glyphicon-map-marker"></span> {pos}</p>
+					<p className="entry-info"><span className="glyphicon glyphicon-calendar"></span> {date}</p>
+					<p className="entry-info"><span className="glyphicon glyphicon-mail"></span> {this.props.entry.mail}</p>
 
 					<div className="entry-note input-group">
 						<textarea ref="note" rows="2" className="entry-note-text form-control" placeholder="Domluv se co a jak a přidej svůj kontakt" />
